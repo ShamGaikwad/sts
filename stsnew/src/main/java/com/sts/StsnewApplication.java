7f7fe0d55b59e42d18e0d39cf38050a2180026a6
DELL <DELL@DESKTOP-GIDC9T7>
2024-09-28 15:53:14 +0530

    ok

---
+++ stsnew/src/main/java/com/sts/StsnewApplication.java
@@ -0,0 +1,13 @@
+package com.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsnewApplication.class, args);
+	}
+
+}
